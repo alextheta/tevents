6afaa1e4ca4064d3ac4b09255c26c3c94920a0e7
Alexander <thetarabbit@gmail.com>
2023-04-09 14:16:13 +0400

    inline EventKeyWrapper

--- include/tevents/impl/EventKeyWrapper_Impl.hpp
+++ include/tevents/impl/EventKeyWrapper_Impl.hpp
@@ -1,24 +1,25 @@
 #pragma once
 
+#include <string>
 #include <tevents/EventKeyWrapper.hpp>
 
 namespace te
 {
-    EventKeyWrapper::EventKeyWrapper(void *objectPtr, std::shared_ptr<uint8_t[]> callbackPtr, size_t callbackSize) :
+    inline EventKeyWrapper::EventKeyWrapper(void *objectPtr, std::shared_ptr<uint8_t[]> callbackPtr, size_t callbackSize) :
             _objectPtr(objectPtr),
             _callbackPtr(callbackPtr),
             _callbackSize(callbackSize)
     {
     }
 
-    bool EventKeyWrapper::operator==(const EventKeyWrapper &other) const
+    inline bool EventKeyWrapper::operator==(const EventKeyWrapper &other) const
     {
         return other._objectPtr == _objectPtr &&
                other._callbackSize == _callbackSize &&
                std::memcmp(other._callbackPtr.get(), _callbackPtr.get(), _callbackSize) == 0;
     }
 
-    size_t EventKeyWrapper::Hash::operator()(const EventKeyWrapper &eventKeyWrapper) const
+    inline size_t EventKeyWrapper::Hash::operator()(const EventKeyWrapper &eventKeyWrapper) const
     {
         std::string s((char *) eventKeyWrapper._callbackPtr.get(), eventKeyWrapper._callbackSize);
         auto h1 = std::hash<std::string>{}(s);
